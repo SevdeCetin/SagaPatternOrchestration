afdfd491e8376545f274230ee100e8f9b3b89ea0
Sevda Çetin <58533625+SevdaCetin@users.noreply.github.com>
2021-11-22 16:46:00 +0300

    Revert "Revert "Add project files.""
    This reverts commit e6ce25f80b3752afa9ea02b744df535e6c603c30.

---
+++ Shared/Interfaces/Messages/StockRollBackMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces.Messages
+{
+    public class StockRollBackMessage : IStockRollBack
+    {
+        public List<OrderItemMessage> OrderItems { get; set; }
+    }
+}
