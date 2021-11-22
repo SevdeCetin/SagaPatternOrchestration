0fa7fe18b516d6258670fc2b21644cdbe2562be1
sevda.cetin <sevda.cetin@MSKK-DAN-LT9135>
2021-11-22 16:22:40 +0300

    Add project files.

---
+++ Shared/RabbitMQSettingsConst.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class RabbitMQSettingsConst
+    {
+        public const string OrderSaga = "order-saga-queue";
+        public const string StockRollBackMessageQueueName = "stock-rollback-queue";
+
+        public const string StockOrderCreatedEventQueueName = "stock-order-created-queue";
+
+        public const string StockPaymentFailedEventQueueName = "stock-payment-failed-queue";
+
+        public const string PaymentStockReservedRequestQueueName = "payment-stock-reserved-request-queue";
+        public const string OrderRequestCompletedEventQueueName = "order-request-completed-queue";
+        public const string OrderRequestFailedEventQueueName = "order-request-failed-queue";
+    }
+}
